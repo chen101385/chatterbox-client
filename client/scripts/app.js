5e51d1416f6b12ce69ad79243a50e448c3221879
chen101385 <chen1385@gmail.com>
2017-12-08 21:46:22 -0800

    implement methods to populate chatrooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,32 @@
-// YOUR CODE HERE:
 class App {
   constructor() {
-    this.init();
     this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+    this.init();
+    this.fetch();
+    this.roomNames = [];
+    this.currentMessages = [];
+    // setInterval(() => this.fetch(), 5000);
+    
   }
   
   init() {
     $(document).ready(function() {
       $('#main').on('click', '.username', app.handleUsernameClick);
-      $('.submit').on('submit', app.handleSubmit);  
+      // $('.submit').click(app.handleSubmit);
+      $('#main').on('click', '.clear', app.clearMessages);
+      $('#main').on('click', '.submit', app.handleSubmit);
+      $('#main').on('click', '.addRoom', app.addRoom);
+      $('#main').on('click', '.room', function() {
+        app.clearMessages();
+        let roomData = _.filter(currentMessages, function(message) {
+          message.roomname === $(this).val();
+        });
+        app.renderData(roomData);   
+      });
+      /*
+      $('#main').on('click', '.room', app.changeRoom);
+       on click/select of roomname, filter data to only show messages match roomname  
+      */
     });
   }
   
@@ -17,11 +35,13 @@ class App {
   // This is the url you should use to communicate with the parse API server.
       url: this.server,
       type: 'POST',
-      data: message,
+      data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
         console.log(data);
+        app.clearMessages();
+        app.fetch();
       },
       error: function (data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -33,11 +53,16 @@ class App {
   fetch() {
     $.ajax({
   // This is the url you should use to communicate with the parse API server.
-      url: this.server,
+      url: `${this.server}?order=-updatedAt`,
       type: 'GET',
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message fetched');
+        console.log(data);
+        let safeArr = [];
+        data.results.forEach((message) => safeArr.push(xssEscape(message)));
+        safeArr.forEach((message) => app.currentMessages.push(message));
+        app.renderData(data);
       },
       error: function (data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -49,15 +74,18 @@ class App {
   clearMessages() {
     $('#chats').empty();
   }
+  
+  renderData(data) {
+    for (var i = 0; i < data.results.length; i++) {          
+      app.renderMessage(data.results[i]);  
+    }
+  }
+  
   renderMessage(message) {
     $('#chats').append(`<div class="messages"><a href="#" class="username">${message.username}</a>: ${message.text}</div>`);
     console.log(`${message.text}`);
   }
-  // var message = {
-  // username: 'shawndrost',
-  // text: 'trololo',
-  // roomname: '4chan'
-  //}
+  
   renderRoom(roomName) {
     $('#roomSelect').prepend(`<option value="${roomName}"> ${roomName} </option>`);
   }
@@ -67,13 +95,30 @@ class App {
   }
   
   handleSubmit() {
-    console.log('you reached me');
+    let username = window.location.search.slice(10);
+    let message = $('#message').val();
+    var obj = {};
+    
+    obj.text = message;
+    obj.username = username;
+    app.send(obj);    
   }
-  //add a friend upon clicking their username
-    //add event handler on 'click'
-      //jquery method to add friend name to a container
-  
-  
+  addRoom() {
+    //retrieve value from the input field
+      //check value against array of existing values
+        //if non-existent, then push to array;
+          //array values reflect <option> values in HTML
+    let inputRoomName = $('#message').val();
+    console.log(inputRoomName);
+    if (inputRoomName.length && app.roomNames.indexOf(inputRoomName) === -1) {
+      app.roomNames.push(inputRoomName);
+      console.log(app.roomNames);
+      $('#roomSelect').prepend(`<option class="room" value="${inputRoomName}"> ${inputRoomName} </option>`);
+    } else {
+      alert('Room already exists!');
+    }  
+  }
 }
 
-var app = new App;
+var app = new App();
+
